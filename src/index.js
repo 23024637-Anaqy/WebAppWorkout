7f324676b364ebb0c005aa46d790c6e20b7ecde4
23024637-Anaqy <23024637@myrp.edu.sg>
2025-01-24 16:10:36 +0800

    latest commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { WorkoutContextProvider } from "./context/WorkoutContext";
+import { WorkoutContextProvider } from "./src/context/WorkoutContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
